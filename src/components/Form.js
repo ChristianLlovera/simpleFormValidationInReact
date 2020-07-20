bf1bd7241154a1db6b8ada9c113d5b2b032520ff
ChristianLlovera <christian.llovera@gmail.com>
2020-07-20 19:49:29 -0400

    new changes in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,67 +1,48 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useRef } from 'react'
+import Validator from 'validatorjs'
 
-const InputText = props => {
-
-    const { name, type } = props
-
-    const [value, setValue] = useState('')
-
-    return (
-        <input type="text" placeholder={name} ref={type} value={value} onChange={e => setValue(e.target.value)} />
-    )
-}
 
 const Form = props => {
 
     const { setValid, setSubmit } = props
+    const formRef = useRef()
 
-    const name = useRef()
-    const email = useRef()
-    const phone = useRef()
-    const url = useRef()
+    const getFormData = () => {
+        const inputs = formRef.current.querySelectorAll('input')
+        const data = {}
+
+        Object.keys(inputs).map(element => {
+            data[inputs[element].name] = inputs[element].value
+        })
+
+        return data
+    }
+
 
     const validate = () => {
         setSubmit(true)
+        const data = getFormData()
 
-        const regxEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-        const regxPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
-        const regxUrl = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
-        const regxName = /^[a-zA-Z ]{2,30}$/
-
-
-        if (!regxName.test(name.current.value)) {
-            setValid(false)
-            return false
+        const rules = {
+            name: 'required|string',
+            phone: 'required|numeric',
+            email: 'required|email',
+            url: 'required|url'
         }
 
-        if (!regxEmail.test(email.current.value)) {
-            setValid(false)
-            return false
-        }
-
-        if (!regxPhone.test(phone.current.value)) {
-            setValid(false)
-            return false
-        }
-
-        if (!regxUrl.test(url.current.value)) {
-            setValid(false)
-            return false
-        }
-
-        setValid(true)
+        const validate = new Validator(data, rules)
+        validate.fails() ? setValid(false) : setValid(true)
 
     }
 
     return (
         <div className="row">
             <h1 className="text-center">Form Validation</h1>
-            <form>
-
-                <InputText name='Name' type={name} />
-                <InputText name='Email' type={email} />
-                <InputText name='Phone' type={phone} />
-                <InputText name='Url' type={url} />
+            <form ref={formRef}>
+                <input type="text" placeholder={'Name'} name='name' />
+                <input type="email" placeholder={'Email'} name='email' />
+                <input type="tel" placeholder={'Phone'} name='phone' />
+                <input type="url" placeholder={'Url'} name='url' />
 
                 <div className="small-6 small-centered text-center columns">
                     <a href="#" className="button success expand round text-center" onClick={() => validate()} >Verify</a>
